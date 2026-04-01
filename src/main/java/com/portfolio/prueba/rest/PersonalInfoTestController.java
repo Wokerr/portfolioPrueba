ad794b0f2ab243b285863b11c53797c2bce100cf
Wokerr <emmanuelare00@gmail.com>
2026-04-01 11:53:25 -0500

    feat: add Put and Delete endpoint to PersonalInfoTestController

--- src/main/java/com/portfolio/prueba/rest/PersonalInfoTestController.java
+++ src/main/java/com/portfolio/prueba/rest/PersonalInfoTestController.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -47,7 +49,19 @@ public class PersonalInfoTestController {
     @PostMapping
     public ResponseEntity<PersonalInfo> createPerosnalInfo(@RequestBody PersonalInfo personalInfo) {
         PersonalInfo newPersonalInfo = personalInfoService.save(personalInfo);
-        
+
         return new ResponseEntity<>(newPersonalInfo, HttpStatus.CREATED);
+    }
+    
+    @PutMapping("/{id}")
+    public PersonalInfo update(@PathVariable Long id, @RequestBody PersonalInfo personalInfo) {
+        personalInfo.setId(id);
+
+        return personalInfoService.save(personalInfo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBy(@PathVariable Long id){
+        personalInfoService.deleteById(id);
     }
 }
