4136eaadb896cc7fd0eef14878baf1a4ad463dcc
Wokerr <emmanuelare00@gmail.com>
2026-04-02 08:27:31 -0500

    feat(controller): Add SkillController

---
+++ src/main/java/com/portfolio/prueba/rest/SkillController.java
@@ -0,0 +1,47 @@
+package com.portfolio.prueba.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import com.portfolio.prueba.model.Skill;
+import com.portfolio.prueba.service.ISkillService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/skill")
+@RequiredArgsConstructor
+public class SkillController {
+    private final ISkillService skillService;
+
+    @GetMapping
+    public List<Skill> findAll() {
+        return skillService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Skill> findById(@PathVariable Long id) {
+        return skillService.findById(id);
+    }
+    @GetMapping("/personal-info/{id}")
+    public List<Skill> findSkillsByPersonalInfoId(@PathVariable("id") Long personalInfoId){
+        return skillService.findByPersonalInfoId(personalInfoId);
+    }
+
+    @PostMapping
+    public Skill save(@RequestBody Skill skill) {
+        return skillService.save(skill);
+    }
+
+    @PutMapping("/{id}")
+    public Skill update(@PathVariable Long id, @RequestBody Skill skill) {
+        skill.setId(id);
+        return skillService.save(skill);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
+        skillService.deleteById(id);
+    }
+}
