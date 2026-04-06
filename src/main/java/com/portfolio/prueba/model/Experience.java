9ce0abd1f31ac56297083af1495a6c1a33be1d87
Woker <emmanuelare00@gmail.com>
2026-04-06 08:32:27 -0500

    feat(Experience): add validations

--- src/main/java/com/portfolio/prueba/model/Experience.java
+++ src/main/java/com/portfolio/prueba/model/Experience.java
@@ -2,6 +2,9 @@ package com.portfolio.prueba.model;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,11 +16,22 @@ import lombok.NoArgsConstructor;
 public class Experience {
 
     private Long id;
+
+    @NotBlank(message = "Field job tittle cannot be empty")
     private String jobTitle;
+    @NotBlank(message = "Field company name tittle cannot be empty")
     private String companyName;
+    
+    @NotNull
+    @PastOrPresent
     private LocalDate startDate;
+    
+    @PastOrPresent
     private LocalDate endDate;
+
+    @NotBlank(message = "Field description tittle cannot be empty")
     private String description;
+
     private Long personalInfoId; // Foreing key to PersonalInfo
 
 }
