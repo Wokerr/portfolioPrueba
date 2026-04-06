20a4d50b07831fa495f049cfb232dcb4b1b836c3
Woker <emmanuelare00@gmail.com>
2026-04-06 07:54:51 -0500

    feat(Skill): Add validations

--- src/main/java/com/portfolio/prueba/model/Skill.java
+++ src/main/java/com/portfolio/prueba/model/Skill.java
@@ -1,5 +1,9 @@
 package com.portfolio.prueba.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +14,15 @@ import lombok.NoArgsConstructor;
 
 public class Skill {
     private Long id;
+    @NotBlank(message = "Field name cannot be empty")
     private String name;
+
+    @NotNull
+    @Min(value = 0)
+    @Max(value = 100)
     private Integer levelPercentage; // Progress Bar
+
+    @NotBlank(message = "You must have an Icon")
     private String iconClass;
     private Long perosnalInfo; // Foreing key to PersonalInfo
 }
