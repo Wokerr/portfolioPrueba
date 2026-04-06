553620f0a86abc10aff0f82585e0ead986a2b225
Woker <emmanuelare00@gmail.com>
2026-04-06 08:17:44 -0500

    feat(Education): Add validations

--- src/main/java/com/portfolio/prueba/model/Education.java
+++ src/main/java/com/portfolio/prueba/model/Education.java
@@ -2,6 +2,11 @@ package com.portfolio.prueba.model;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,10 +17,21 @@ import lombok.NoArgsConstructor;
 
 public class Education {
     private Long id;
+
+    @NotBlank(message = "Field degree cannot be empty")
     private String degree;
+
+    @NotBlank(message = "Field institution cannot be empty")
     private String institution;
+    
+    @NotNull
+    @PastOrPresent
     private LocalDate startDate;
+    
+    @PastOrPresent
     private LocalDate endDate; // Can be null if its in progress
+    
+    @NotBlank(message = "Field description cannot be empty")
     private String description;
     private Long PersonalInfoId; // Foreing key to PersonalInfo
 }
