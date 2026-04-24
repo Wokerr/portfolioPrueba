7b4f478ec2a5bf281d385bf191129b378911eca6
Woker <emmanuelare00@gmail.com>
2026-04-23 20:02:07 -0500

    refactor(SkillController): Add rute '/' to get all skills

--- src/main/java/com/portfolio/prueba/rest/SkillController.java
+++ src/main/java/com/portfolio/prueba/rest/SkillController.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 public class SkillController {
     private final ISkillService skillService;
 
-    @GetMapping
+    @GetMapping({"", "/"})
     public List<Skill> findAll() {
         return skillService.findAll();
     }
